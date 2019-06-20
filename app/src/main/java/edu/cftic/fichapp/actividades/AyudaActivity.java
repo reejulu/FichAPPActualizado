195125c8f8f6cb5f27e721a8ffad2dd56b39c186
alexomonty <monto.alex@gmail.com>
2019-06-20 13:43:36 +0200

    Commit arreglo DB, ayuda ,cursores

--- app/src/main/java/edu/cftic/fichapp/actividades/AyudaActivity.java
+++ app/src/main/java/edu/cftic/fichapp/actividades/AyudaActivity.java
@@ -15,11 +15,15 @@ import edu.cftic.fichapp.util.Preferencias;
 public class AyudaActivity extends AppCompatActivity {
     private boolean estado_barra = false;
 
+    private boolean viene_de_menu;
 
+    //TODO mejora, añadir la barra de navegación hacia atrás
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ayuda);
+
+        viene_de_menu = getIntent().getBooleanExtra("vengo_de_menu", false);
 
         //Cargar el fichero HTML en el web view
         WebView wv = (WebView) findViewById(R.id.webView2);
@@ -34,9 +38,15 @@ public class AyudaActivity extends AppCompatActivity {
     }
 
     public void aceptarAyuda(View view) {
-        Log.d(Constantes.TAG_APP, "Transitando a Login desde Ayuda");
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+        Intent intent_salida = null;
+        if (!viene_de_menu)
+        {
+            Log.d(Constantes.TAG_APP, "Transitando a Login desde Ayuda");
+            intent_salida = new Intent(this, LoginActivity.class);
+            startActivity(intent_salida);
+        }
+        
+
+        finish(); //si vengo de menú, simplemente cierro
     }
 }
